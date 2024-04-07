0632c1ecff0e7894e0b3027eab6cce74e5e2c487
Preetraj Gupta <preetrajgupta2002@gmail.com>
2024-04-07 11:06:47 +0530

    adding scrollbar

--- gui.py
+++ gui.py
@@ -14,7 +14,7 @@ AshGray="#C0C0C0"
 FONT_NAME = "Courier"
 
 def load_module():
-    run_command()
+    run_command("")
 
 def remove_module():
     run_command("ls -l")
@@ -94,53 +94,79 @@ def run_command(command=""):
 
 window = Tk()
 window.title("Netfilter Firewall")
-window.geometry('900x1200')
-window.config(padx=100, pady=20, bg=Glaucous)
+window.geometry('840x900')
+# window.config(padx=100, pady=20, bg=Glaucous)
+
+#Create a main frame
+main_frame = Frame(window)
+main_frame.pack(fill=BOTH,expand=1)
+
+#Create a canvas 
+my_canvas = Canvas(main_frame)
+my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+
+#Add a scroll bar to the canvas
+my_scrollbar = Scrollbar(main_frame,orient=VERTICAL, command=my_canvas.yview)
+my_scrollbar.pack(side=RIGHT, fill=Y)
+
+#Configure the canvas
+my_canvas.configure(yscrollcommand=my_scrollbar.set)
+my_canvas.bind("<Configure>",lambda e: my_canvas.config(scrollregion=my_canvas.bbox("all")))
+# main_frame.bind_all("<MouseWheel>", lambda event: my_canvas.yview_scroll(-1*(event.delta//120), "units"))
+
+#Create a 2nd frame inside canvas
+second_frame = Frame(my_canvas)
+second_frame.config(padx=50, pady=20, bg=Glaucous)
+
+#Add that new frame to a window inside canvas
+my_canvas.create_window((0,0),window=second_frame,anchor="ne")
+
+
 
 # Label
-title = Label(text="Netfilter Firewall", font=(FONT_NAME, 25, "bold"), bg=Glaucous, fg=GREEN)
+title = Label(second_frame,text="Netfilter Firewall", font=(FONT_NAME, 25, "bold"), bg=Glaucous, fg=GREEN)
 title.pack(pady=10)
-ip_label = Label(window, text="Enter IP addresses:",font=("Arial", 12, "bold"), bg=Glaucous, fg=BLACK)
+ip_label = Label(second_frame, text="Enter IP addresses:",font=("Arial", 12, "bold"), bg=Glaucous, fg=BLACK)
 ip_label.pack()
 
 
 # Entry
-ip_text = Text(window, height = 1, width = 30, bg = "light cyan")
+ip_text = Text(second_frame, height = 1, width = 30, bg = "light cyan")
 ip_text.pack()
 
 # Buttons
-load_but = Button(text="Load Module", width=12, command=load_module)
+load_but = Button(second_frame,text="Load Module", width=12, command=load_module)
 load_but.pack(pady=10)
-remove_but = Button(text="Remove Module", width=12,command=remove_module)
+remove_but = Button(second_frame,text="Remove Module", width=12,command=remove_module)
 remove_but.pack(pady=10)
-# list_but = Button(text="List Module", width=12, command=list_module)
+# list_but = Button(second_frame,text="List Module", width=12, command=list_module)
 # list_but.pack(pady=10)
-log_but = Button(text="Show log", width=12,command=show_log)
+log_but = Button(second_frame,text="Show log", width=12,command=show_log)
 log_but.pack(pady=10)
 
-refresh_button = Button(window, text="Refresh dmesg Logs", command=show_log)
+refresh_button = Button(second_frame, text="Refresh dmesg Logs", command=show_log)
 refresh_button.pack(pady=10)
 
-# dmesg_text = Text(window, height=15, width=100,bg = AshGray)
-# dmesg_text.pack(pady=10)
+dmesg_text = Text(second_frame, height=15, width=80,bg = AshGray)
+dmesg_text.pack(pady=10)
 
 # Terminal Section
-command_entry = Entry(window, width=50,bg = AshGray)
+command_entry = Entry(second_frame, width=50,bg = AshGray)
 command_entry.bind('<Return>', enter_run)
 command_entry.pack(pady=10)
 
 
-run_button = Button(window, text="Run Command", command=run_command)
+run_button = Button(second_frame, text="Run Command", command=run_command)
 run_button.pack(pady=10)
 
-terminal_output = Text(window, height=10, width=100,bg = AshGray)
+terminal_output = Text(second_frame, height=10, width=80,bg = AshGray)
 terminal_output.tag_configure("green", foreground="green")
 terminal_output.tag_configure("blue", foreground="blue")
 terminal_output.tag_configure("red", foreground="red")
 terminal_output.pack(pady=10)
 
 
-exit = Button(window, text = "Exit", width = 12, command = window.destroy)
+exit = Button(second_frame, text = "Exit", width = 12, command = window.destroy)
 exit.pack(pady=10)
 
 # show_log()
